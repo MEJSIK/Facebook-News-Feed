913174ee065619f13fa2d34e2f0621aa72724507
MEJS <bartekwalasik@gmail.com>
2018-01-09 15:26:12 +0100

    v2.0
    Added photo,  createtime and link to post on facebook.

--- js/feed.js
+++ js/feed.js
@@ -1,14 +1,15 @@
 var app = angular.module('facebookFeed', []);
-var pageId = '[PAGE-ID]'; //Your PageID
-var token = '[APP-ID]]|[SECRET-CODE]'; //Your Access Token
+
+var pageId = '153994084663979'; //Your PageID
+var token = '1929704930624447|ZL6GwBi0o1eTrtFCYoJ0Phgr4FA'; //Your Access Token
 
 
 // Request below GET every post using Graph API
 app.controller('facebookFeedControler', function ($scope, $http) {
-    $http.get(`https://graph.facebook.com/${pageId}/?fields=posts.limit(10){message,likes.limit(0).summary(1),created_time}&access_token=${token}`)
+    $http.get(`https://graph.facebook.com/${pageId}/?fields=posts.limit(10){message,likes.limit(0).summary(1),created_time,comments.limit(2),full_picture}&access_token=${token}`)
         .then(function (response) {
             $scope.posts = response.data.posts.data;
+            a = $scope.posts[0].created_time;
+            console.log(a.substr(0, 10));
         });
-
-
 });
